390caa6187ccfd5cac13c8f3700cdd0ff99ddc15
ViduraDa <vidurada@gmail.com>
2018-04-01 20:45:38 +0530

    login and dashboard interface

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'vidura',
+        'password': 'vidura'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
